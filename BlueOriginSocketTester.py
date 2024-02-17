9eec0587cbc1403ec4ddd989dbfd5c543737cb0f
MattoScientisuto <gmstegnokilla@gmail.com>
2024-02-16 17:20:20 -0800

    Added 'Sequence of Events' document for python cases

--- BlueOriginSocketTester.py
+++ BlueOriginSocketTester.py
@@ -1,7 +1,7 @@
 import socket
 
-HOST = '127.0.0.1'  # Listen on all network interfaces
-PORT = 50155  # Choose a port for communication
+HOST = '127.0.0.1'  # Loopback address
+PORT = 50155        # Port number your server is listening on
 
 # Create a TCP socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
@@ -19,7 +19,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     with connection:
         print("Connected to:", client_address)
 
-        # Receive data from the client
+        # Receive and process messages indefinitely
         while True:
             data = connection.recv(1024)
             if not data:
--- start_MessageSender.bat
+++ start_MessageSender.bat
@@ -1,1 +1,1 @@
-python "C:\Users\mattduong\Documents\MATT SPARTA SOFTWARE\BlueOriginMessageSender.py"
+python "BlueOriginMessageSender.py"
